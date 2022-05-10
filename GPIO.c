43af5306331b5e95cfcc21cf5b976ee4b6739859
Menna-Elkhwass <105253678+Menna-Elkhwass@users.noreply.github.com>
2022-05-11 00:24:33 +0200

    Update GPIO.c

--- GPIO.c
+++ GPIO.c
@@ -5,57 +5,57 @@ void GPIO_PORT_INT(unsigned char PORTNAME)
   switch(PORTNAME)
   {
   case GPIO_PORTA:
-	  { SET_BIT(SYSCTL_RCGCGPIO_R, GPIO_PORTA);     //Clock Enable for port A
+    { SET_BIT(SYSCTL_RCGCGPIO_R, GPIO_PORTA);     //Clock Enable for port A
       while(READ_BIT(SYSCTL_PRGPIO_R, 0) == 0);  //allow time for clock to start
       GPIO_PORTA_LOCK_R = 0x4C4F434B;           //unlock PORTA register
       GPIO_PORTA_CR_R = 0xFF;                  //Allow changes in Port A
       GPIO_PORTA_DEN_R = 0xFF;	              //Enable digital for port A
-		}	
+    }	
     break;
 
   case GPIO_PORTB:
-	  { SET_BIT(SYSCTL_RCGCGPIO_R, GPIO_PORTB);     //Clock Enable for port B
+    { SET_BIT(SYSCTL_RCGCGPIO_R, GPIO_PORTB);     //Clock Enable for port B
       while(READ_BIT(SYSCTL_PRGPIO_R, 1) == 0);  //allow time for clock to start
       GPIO_PORTB_LOCK_R = 0x4C4F434B;           //unlock PORTB register
-      GPIO_PORTB_CR_R = 0xFF; 				         //Allow changes in Port B
-		  GPIO_PORTB_DEN_R = 0xFF;				        //Enable digital for port B
-		}	
+      GPIO_PORTB_CR_R = 0xFF; 		       //Allow changes in Port B
+      GPIO_PORTB_DEN_R = 0xFF;		      //Enable digital for port B
+    }	
     break;
 
   case GPIO_PORTC:
     { SET_BIT(SYSCTL_RCGCGPIO_R, GPIO_PORTC);     //Clock Enable for port C
       while(READ_BIT(SYSCTL_PRGPIO_R, 2) == 0);  //allow time for clock to start
-      GPIO_PORTC_LOCK_R = 0x4C4F434B; 		      //unlock PORT C register
-      GPIO_PORTC_CR_R |= 0xF0; 				         //Allow changes in Port C for pins4-7 only The others are used JTAG on the board
-      GPIO_PORTC_DEN_R |= 0xF0;	 			        //Enable digital for port C
-		}	
+      GPIO_PORTC_LOCK_R = 0x4C4F434B; 		//unlock PORT C register
+      GPIO_PORTC_CR_R |= 0xF0;                 //Allow changes in Port C for pins4-7 only The others are used JTAG on the board
+      GPIO_PORTC_DEN_R |= 0xF0;	 	      //Enable digital for port C
+    }	
     break;
 
   case GPIO_PORTD:
-	  { SET_BIT(SYSCTL_RCGCGPIO_R, GPIO_PORTD);     //Clock Enable for port D
+    { SET_BIT(SYSCTL_RCGCGPIO_R, GPIO_PORTD);     //Clock Enable for port D
       while(READ_BIT(SYSCTL_PRGPIO_R, 3) == 0);  //allow time for clock to start
-      GPIO_PORTD_LOCK_R = 0x4C4F434B; 		      //unlock PORTD register
-      GPIO_PORTD_CR_R = 0xFF; 				         //Allow changes in Port D
-		  GPIO_PORTD_DEN_R = 0xFF;				        //Enable digital for port D
-		}	
+      GPIO_PORTD_LOCK_R = 0x4C4F434B; 		//unlock PORTD register
+      GPIO_PORTD_CR_R = 0xFF; 		       //Allow changes in Port D
+      GPIO_PORTD_DEN_R = 0xFF;	              //Enable digital for port D
+    }	
     break;
 
   case GPIO_PORTE:
-	  { SET_BIT(SYSCTL_RCGCGPIO_R, GPIO_PORTE);     //Clock Enable for port E
+    { SET_BIT(SYSCTL_RCGCGPIO_R, GPIO_PORTE);     //Clock Enable for port E
       while(READ_BIT(SYSCTL_PRGPIO_R, 4) == 0);  //allow time for clock to start
       GPIO_PORTE_LOCK_R = 0x4C4F434B;           //unlock PORTE register
       GPIO_PORTE_CR_R = 0xFF;                  //Allow changes in Port E
-		  GPIO_PORTE_DEN_R = 0xFF;                //Enable digital for port E
-		}	
+      GPIO_PORTE_DEN_R = 0xFF;                //Enable digital for port E
+    }	
     break;
 
   case GPIO_PORTF:
-	  { SET_BIT(SYSCTL_RCGCGPIO_R, GPIO_PORTF);     //Clock Enable for port F
+    { SET_BIT(SYSCTL_RCGCGPIO_R, GPIO_PORTF);     //Clock Enable for port F
       while(READ_BIT(SYSCTL_PRGPIO_R, 5) == 0);  //allow time for clock to start
       GPIO_PORTF_LOCK_R = 0x4C4F434B;           //unlock PORTF register
       GPIO_PORTF_CR_R = 0xFF;                  //Allow changes to Port F
-		  GPIO_PORTF_DEN_R = 0xFF;                //Enable digital for port F
-	  }		
+      GPIO_PORTF_DEN_R = 0xFF;                //Enable digital for port F
+    }		
     break;
   }
 }
@@ -139,7 +139,7 @@ void SET_PIN_DIRECTION(unsigned char PORTNAME, unsigned char PINNUMBER, unsigned
 void WRITE_PIN_VALUE(unsigned char PORTNAME, unsigned char PINNUMBER, unsigned char VALUE)
 {
  switch(PORTNAME)
-  {
+ {
   case GPIO_PORTA:   //Write Data to Pin in portA
    {
     if(VALUE==1)
@@ -225,47 +225,45 @@ void WRITE_PIN_VALUE(unsigned char PORTNAME, unsigned char PINNUMBER, unsigned c
 unsigned char READ_PIN_VALUE(unsigned char PORTNAME, unsigned char PINNUMBER)
 {
  unsigned char return_value; //Variable to store the return value of the function
-
   switch(PORTNAME)
   {
     case GPIO_PORTA:   //Read Data from Pin in portA
-	{
-      return_value = READ_BIT(GPIO_PORTA_DATA_R, PINNUMBER);
-	}	
-    break;
+     {
+       return_value = READ_BIT(GPIO_PORTA_DATA_R, PINNUMBER);
+     }	
+     break;
 
     case GPIO_PORTB:   //Read Data from Pin in portB
-	{
-      return_value = READ_BIT(GPIO_PORTB_DATA_R, PINNUMBER);
-	}	
-    break;
+     {
+       return_value = READ_BIT(GPIO_PORTB_DATA_R, PINNUMBER);
+     }	
+     break;
 
     case GPIO_PORTC:   //Read Data from Pin in portC
-	{
-      return_value = READ_BIT(GPIO_PORTC_DATA_R, PINNUMBER);
-	}	
-    break;
+     {
+       return_value = READ_BIT(GPIO_PORTC_DATA_R, PINNUMBER);
+     }	
+     break;
 
     case GPIO_PORTD:   //Read Data from Pin in portD
-	{
-      return_value = READ_BIT(GPIO_PORTD_DATA_R, PINNUMBER);
-	}
-    break;
+     {
+       return_value = READ_BIT(GPIO_PORTD_DATA_R, PINNUMBER);
+     }
+     break;
 
     case GPIO_PORTE:   //Read Data from Pin in portE
-	{
-      return_value = READ_BIT(GPIO_PORTE_DATA_R, PINNUMBER);
-	}
-    break;
+     {
+       return_value = READ_BIT(GPIO_PORTE_DATA_R, PINNUMBER);
+     }
+     break;
 
     case GPIO_PORTF:   //Read Data from Pin in portF
-	{
-      return_value = READ_BIT(GPIO_PORTF_DATA_R, PINNUMBER);
-	}
-    break;
+     {
+       return_value = READ_BIT(GPIO_PORTF_DATA_R, PINNUMBER);
+     }
+     break;
   }
-
-	return return_value;
+return return_value;
 }
 
 //*******************************************************************************************************************************
@@ -273,38 +271,38 @@ unsigned char READ_PIN_VALUE(unsigned char PORTNAME, unsigned char PINNUMBER)
 void TOGGLE_PIN(unsigned char PORTNAME, unsigned char PINNUMBER)
 {
 switch(PORTNAME)
-  {
-    case GPIO_PORTA: 
+{
+   case GPIO_PORTA: 
     {
       TOG_BIT(GPIO_PORTA_DATA_R, PINNUMBER); //To invert Pin in PortA
     }
     break;
 
-    case GPIO_PORTB:
+   case GPIO_PORTB:
     {
       TOG_BIT(GPIO_PORTB_DATA_R, PINNUMBER); //To invert Pin in PortB
     }
     break;
 
-    case GPIO_PORTC:
+   case GPIO_PORTC:
     {
       TOG_BIT(GPIO_PORTC_DATA_R, PINNUMBER); //To invert Pin in PortC
     }
     break;
 
-    case GPIO_PORTD:
+   case GPIO_PORTD:
     {
       TOG_BIT(GPIO_PORTD_DATA_R, PINNUMBER); //To invert Pin in PortD
     }
     break;
 
-    case GPIO_PORTE:
+   case GPIO_PORTE:
     {
       TOG_BIT(GPIO_PORTE_DATA_R, PINNUMBER); //To invert Pin in PortE
     }
     break;
 
-    case GPIO_PORTF:
+   case GPIO_PORTF:
     {
       TOG_BIT(GPIO_PORTF_DATA_R, PINNUMBER); //To invert Pin in PortF
     }
@@ -324,31 +322,31 @@ void SET_PORT_DIRECTION(unsigned char PORTNAME, unsigned char DIRECTION)
      }
      break;
 
-		case GPIO_PORTB:
+    case GPIO_PORTB:
      {
        GPIO_PORTB_DIR_R = DIRECTION; //Setting the direction of PortB
      }
      break;
 
-		case GPIO_PORTC:
+    case GPIO_PORTC:
      {
        GPIO_PORTC_DIR_R = DIRECTION; //Setting the direction of PortC
      }
      break;
 
-		case GPIO_PORTD:
+    case GPIO_PORTD:
      {
        GPIO_PORTD_DIR_R = DIRECTION; //Setting the direction of PortD
      }
      break;
 
-		case GPIO_PORTE:
+    case GPIO_PORTE:
      {
        GPIO_PORTE_DIR_R = DIRECTION; //Setting the direction of PortE
      }
      break;
 
-		case GPIO_PORTF:
+    case GPIO_PORTF:
      {
        GPIO_PORTF_DIR_R = DIRECTION; //Setting the direction of PortF
      }
@@ -368,31 +366,31 @@ void WRITE_PORT_VALUE(unsigned char PORTNAME, unsigned char VALUE)
      }
      break;
 
-		case GPIO_PORTB:
+    case GPIO_PORTB:
      {
        GPIO_PORTB_DATA_R = VALUE; //Writing data on PortB
      }
      break;
 
-		case GPIO_PORTC:
+    case GPIO_PORTC:
      {
        GPIO_PORTC_DATA_R = VALUE; //Writing data on PortC
      }
      break;
 
-		case GPIO_PORTD:
+    case GPIO_PORTD:
      {
        GPIO_PORTD_DATA_R = VALUE; //Writing data on PortD
      }
      break;
 
-		case GPIO_PORTE:
+    case GPIO_PORTE:
      {
        GPIO_PORTE_DATA_R = VALUE; //Writing data on PortE
      }
      break;
 
-		case GPIO_PORTF:
+    case GPIO_PORTF:
      {
        GPIO_PORTF_DATA_R = VALUE; //Writing data on PortF
      }
@@ -406,7 +404,7 @@ unsigned char READ_PORT(unsigned char PORTNAME)
 {
   unsigned char return_value;
   switch(PORTNAME)
-   {
+  {
     case GPIO_PORTA:
      {
        return_value = GPIO_PORTA_DATA_R; //Read Data of portA
@@ -442,9 +440,9 @@ unsigned char READ_PORT(unsigned char PORTNAME)
        return_value = GPIO_PORTF_DATA_R; //Read Data of portF
      }
      break;
-   }
+  }
 
-	return return_value;
+return return_value;
 }
 
 //*******************************************************************************************************************************
@@ -452,7 +450,7 @@ unsigned char READ_PORT(unsigned char PORTNAME)
 void ENABLE_PULLUP(unsigned char PORTNAME, unsigned char PINNUMBER)
 {
   switch(PORTNAME)
-   {
+  {
     case GPIO_PORTA:
      {
        SET_BIT(GPIO_PORTA_PUR_R, PINNUMBER); //Setting Pull Up Resistor of PortA
@@ -498,98 +496,96 @@ void WRITE_HIGH_NIPPLE(unsigned char PORTNAME, unsigned char VALUE)
   VALUE <<= 4; //Shifting left the data value by 4 
 
   switch(PORTNAME)
-   {
+  {
     case GPIO_PORTA: //Writing on the most significant 4 bits of PortA
      {
-       GPIO_PORTA_DATA_R &= 0X0F;    //Anding with a mask of ones from bits 4 to 7
-	     GPIO_PORTA_DATA_R |= VALUE;  //ORing with the data value after shifting
+       GPIO_PORTA_DATA_R &= 0X0F;    //ANDing with a mask of zeros from bits 4 to 7
+       GPIO_PORTA_DATA_R |= VALUE;  //ORing with the data value after shifting
      }
      break;
 
-	 case GPIO_PORTB: //Writing on the most significant 4 bits of PortB
+   case GPIO_PORTB: //Writing on the most significant 4 bits of PortB
     {
-      GPIO_PORTB_DATA_R &= 0X0F;    //Anding with a mask of ones from bits 4 to 7
-	    GPIO_PORTB_DATA_R |= VALUE;  //ORing with the data value after shifting
+      GPIO_PORTB_DATA_R &= 0X0F;    //ANDing with a mask of zeros from bits 4 to 7
+      GPIO_PORTB_DATA_R |= VALUE;  //ORing with the data value after shifting
     }
     break;
 
-	 case GPIO_PORTC: //Writing on the most significant 4 bits of PortC
+   case GPIO_PORTC: //Writing on the most significant 4 bits of PortC
     {
-      GPIO_PORTC_DATA_R &= 0X0F;    //Anding with a mask of ones from bits 4 to 7
-	    GPIO_PORTC_DATA_R |= VALUE;  //ORing with the data value after shifting
+      GPIO_PORTC_DATA_R &= 0X0F;    //ANDing with a mask of zeros from bits 4 to 7
+      GPIO_PORTC_DATA_R |= VALUE;  //ORing with the data value after shifting
     }
     break;
 
-	 case GPIO_PORTD: //Writing on the most significant 4 bits of PortD
+   case GPIO_PORTD: //Writing on the most significant 4 bits of PortD
     {
-      GPIO_PORTD_DATA_R &= 0X0F;    //Anding with a mask of ones from bits 4 to 7
-	    GPIO_PORTD_DATA_R |= VALUE;  //ORing with the data value after shifting
+      GPIO_PORTD_DATA_R &= 0X0F;    //ANDing with a mask of zeros from bits 4 to 7
+      GPIO_PORTD_DATA_R |= VALUE;  //ORing with the data value after shifting
     }
     break;
 
-	 case GPIO_PORTE: //Writing on the most significant 4 bits of PortE
+   case GPIO_PORTE: //Writing on the most significant 4 bits of PortE
     {
-      GPIO_PORTE_DATA_R &= 0X0F;    //Anding with a mask of ones from bits 4 to 7
-	    GPIO_PORTE_DATA_R |= VALUE;  //ORing with the data value after shifting
+      GPIO_PORTE_DATA_R &= 0X0F;    //ANDing with a mask of zeros from bits 4 to 7
+      GPIO_PORTE_DATA_R |= VALUE;  //ORing with the data value after shifting
     }
     break;
 
-	 case GPIO_PORTF //Writing on the most significant 4 bits of PortF
+   case GPIO_PORTF //Writing on the most significant 4 bits of PortF
     {
-      GPIO_PORTF_DATA_R &= 0X0F;    //Anding with a mask of ones from bits 4 to 7
-	    GPIO_PORTF_DATA_R |= VALUE;  //ORing with the data value after shifting
+      GPIO_PORTF_DATA_R &= 0X0F;    //ANDing with a mask of zeros from bits 4 to 7
+      GPIO_PORTF_DATA_R |= VALUE;  //ORing with the data value after shifting
     }
     break;
-   }
+  }
 }
 
 //*******************************************************************************************************************************
 
 void WRITE_LOW_NIPPLE(unsigned char PORTNAME, unsigned char VALUE)
 {
-  VALUE <<= 4;
-
-	switch(PORTNAME)
-   {
+  switch(PORTNAME)
+  {
    case GPIO_PORTA: //Writing on the least significant 4 bits of PortA
     {
-      GPIO_PORTA_DATA_R &= 0XF0;    //Anding with a mask of ones from bits 0 to 3
-	    GPIO_PORTA_DATA_R |= VALUE;  //ORing with the data value after shifting
+      GPIO_PORTA_DATA_R &= 0XF0;    //ANDing with a mask of zeros from bits 0 to 3
+      GPIO_PORTA_DATA_R |= VALUE;  //ORing with the data value 
     }
     break;
 
    case GPIO_PORTB: //Writing on the least significant 4 bits of PortB
     {
-      GPIO_PORTB_DATA_R &= 0XF0;    //Anding with a mask of ones from bits 0 to 3
-	    GPIO_PORTB_DATA_R |= VALUE;  //ORing with the data value after shifting
+      GPIO_PORTB_DATA_R &= 0XF0;    //ANDing with a mask of zeros from bits 0 to 3
+      GPIO_PORTB_DATA_R |= VALUE;  //ORing with the data value 
     }
     break;
 
-	 case GPIO_PORTC: //Writing on the least significant 4 bits of PortC
+   case GPIO_PORTC: //Writing on the least significant 4 bits of PortC
     {
-      GPIO_PORTC_DATA_R &= 0XF0;    //Anding with a mask of ones from bits 0 to 3
-	    GPIO_PORTC_DATA_R |= VALUE;  //ORing with the data value after shifting
+      GPIO_PORTC_DATA_R &= 0XF0;    //ANDing with a mask of zeros from bits 0 to 3
+      GPIO_PORTC_DATA_R |= VALUE;  //ORing with the data value 
     }
     break;
 
-	 case GPIO_PORTD: //Writing on the least significant 4 bits of PortD
+   case GPIO_PORTD: //Writing on the least significant 4 bits of PortD
     {
-      GPIO_PORTD_DATA_R &= 0XF0;    //Anding with a mask of ones from bits 0 to 3
-	    GPIO_PORTD_DATA_R |= VALUE;  //ORing with the data value after shifting
+      GPIO_PORTD_DATA_R &= 0XF0;    //ANDing with a mask of zeros from bits 0 to 3
+      GPIO_PORTD_DATA_R |= VALUE;  //ORing with the data value 
     }
     break;
 		
    case GPIO_PORTE: //Writing on the least significant 4 bits of PortE
     {
-      GPIO_PORTE_DATA_R &= 0XF0;    //Anding with a mask of ones from bits 0 to 3
-	    GPIO_PORTE_DATA_R |= VALUE;  //ORing with the data value after shifting
+      GPIO_PORTE_DATA_R &= 0XF0;    //ANDing with a mask of zeros from bits 0 to 3
+      GPIO_PORTE_DATA_R |= VALUE;  //ORing with the data value 
     }
     break;
 
    case GPIO_PORTF: //Writing on the least significant 4 bits of PortF
     {
-      GPIO_PORTF_DATA_R &= 0XF0;    //Anding with a mask of ones from bits 0 to 3
-	    GPIO_PORTF_DATA_R |= VALUE;  //ORing with the data value after shifting
+      GPIO_PORTF_DATA_R &= 0XF0;    //ANDing with a mask of zeros from bits 0 to 3
+      GPIO_PORTF_DATA_R |= VALUE;  //ORing with the data value 
     }
     break;
    }
