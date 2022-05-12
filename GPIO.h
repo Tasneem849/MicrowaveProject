63572ba6b73d90d9f93f5aa7459297838001f3df
Menna-Elkhwass <105253678+Menna-Elkhwass@users.noreply.github.com>
2022-05-12 19:45:51 +0200

    Update GPIO.h

--- GPIO.h
+++ GPIO.h
@@ -1,23 +1,23 @@
 #include "GPIO_MACROS.h"
 
-void GPIO_PORT_INT(unsigned char PORTNAME);
+void GPIO_PORT_INT(unsigned char PORT_NAME);
 
-void SET_PIN_DIRECTION(unsigned char PORTNAME, unsigned char PINNUMBER, unsigned char DIRECTION);
+void SET_PIN_DIRECTION(unsigned char PORT_NAME, unsigned char PIN_NUMBER, unsigned char DIRECTION);
 
-void WRITE_PIN_VALUE(unsigned char PORTNAME, unsigned char PINNUMBER, unsigned char VALUE);
+void WRITE_PIN_VALUE(unsigned char PORT_NAME, unsigned char PIN_NUMBER, unsigned char VALUE);
 
-unsigned char READ_PIN_VALUE(unsigned char PORTNAME, unsigned char PINNUMBER);
+unsigned char READ_PIN_VALUE(unsigned char PORT_NAME, unsigned char PIN_NUMBER);
 
-void TOGGLE_PIN(unsigned char PORTNAME, unsigned char PINNUMBER);
+void TOGGLE_PIN(unsigned char PORT_NAME, unsigned char PIN_NUMBER);
 
-void SET_PORT_DIRECTION(unsigned char PORTNAME, unsigned char DIRECTION);
+void SET_PORT_DIRECTION(unsigned char PORT_NAME, unsigned char DIRECTION);
 
-void WRITE_PORT_VALUE(unsigned char PORTNAME, unsigned char VALUE);
+void WRITE_PORT_VALUE(unsigned char PORT_NAME, unsigned char VALUE);
 
-unsigned char READ_PORT(unsigned char PORTNAME);
+unsigned char READ_PORT(unsigned char PORT_NAME);
 
-void ENABLE_PULLUP(unsigned char PORTNAME, unsigned char PINNUMBER);
+void ENABLE_PULLUP(unsigned char PORT_NAME, unsigned char PIN_NUMBER);
 
-void WRITE_HIGH_NIPPLE(unsigned char PORTNAME, unsigned char VALUE);
+void WRITE_HIGH_NIPPLE(unsigned char PORT_NAME, unsigned char VALUE);
 
-void WRITE_LOW_NIPPLE(unsigned char PORTNAME, unsigned char VALUE);
+void WRITE_LOW_NIPPLE(unsigned char PORT_NAME, unsigned char VALUE);
