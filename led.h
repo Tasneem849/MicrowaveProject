d0dc3715f1d5362de865cbcae706db85d76c0440
Tasneem Mohamed <92854521+Tasneem849@users.noreply.github.com>
2022-05-16 01:01:25 +0200

    Delete led.h

--- led.h
+++
@@ -1,6 +0,0 @@
-#include "GPIO.h"
-void LED_Init(unsigned char PORTNAME , unsigned char PINNUMBER);
-void LED_ON(unsigned char PORTNAME , unsigned char PINNUMBER);
-void LED_OFF(unsigned char PORTNAME , unsigned char PINNUMBER);
-void LED_TOGGLE(unsigned char PORTNAME , unsigned char PINNUMBER);
-
