34e9fc3b3e3f6e10a0aaff859468b0acc9a49e4e
Menna281 <104266111+Menna281@users.noreply.github.com>
2022-05-17 13:01:28 +0200

    Update led.h

--- led.h
+++ led.h
@@ -1,6 +1,6 @@
 #include "GPIO.h"
-void LED_Init(unsigned char PORTNAME , unsigned char PINNUMBER);
-void LED_ON(unsigned char PORTNAME , unsigned char PINNUMBER);
-void LED_OFF(unsigned char PORTNAME , unsigned char PINNUMBER);
-void LED_TOGGLE(unsigned char PORTNAME , unsigned char PINNUMBER);
+void LED_Init(unsigned char PORT_NAME , unsigned char PIN_NUMBER);
+void LED_ON(unsigned char PORT_NAME , unsigned char PIN_NUMBER);
+void LED_OFF(unsigned char PORT_NAME , unsigned char PIN_NUMBER);
+void LED_TOGGLE(unsigned char PORT_NAME , unsigned char PIN_NUMBER);
 
