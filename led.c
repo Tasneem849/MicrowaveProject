c3e5780661576f94489305166034e4f34cc27815
Menna281 <104266111+Menna281@users.noreply.github.com>
2022-05-17 13:00:31 +0200

    Update led.c

--- led.c
+++ led.c
@@ -1,21 +1,21 @@
 #include "led.h"
 
 
-void LED_Init(unsigned char PORTNAME , unsigned char PINNUMBER){
-    GPIO_PORT_INT(PORTNAME);//intialize port
-    SET_PIN_DIRECTION(PORTNAME, PINNUMBER, 1);//set the bin output
+void LED_Init(unsigned char PORT_NAME , unsigned char PIN_NUMBER){
+    GPIO_PORT_INT(PORT_NAME);//intialize port
+    SET_PIN_DIRECTION(PORT_NAME, PIN_NUMBER, 1);//set the bin output
 
 }
-void LED_ON(unsigned char PORTNAME , unsigned char PINNUMBER){
-    WRITE_PIN_VALUE(PORTNAME, PINNUMBER, 1);//turn on the led
+void LED_ON(unsigned char PORT_NAME , unsigned char PIN_NUMBER){
+    WRITE_PIN_VALUE(PORT_NAME, PIN_NUMBER, 1);//turn on the led
     }
-void LED_OFF(unsigned char PORTNAME , unsigned char PINNUMBER){
-    WRITE_PIN_VALUE(PORTNAME, PINNUMBER, 0);//turn off the led
+void LED_OFF(unsigned char PORT_NAME , unsigned char PIN_NUMBER){
+    WRITE_PIN_VALUE(PORT_NAME, PIN_NUMBER, 0);//turn off the led
 
 
 }
-void LED_TOGGLE(unsigned char PORTNAME , unsigned char PINNUMBER) {
-    TOGGLE_PIN(PORTNAME, PINNUMBER);//toggle the led
+void LED_TOGGLE(unsigned char PORT_NAME , unsigned char PIN_NUMBER) {
+    TOGGLE_PIN(PORT_NAME, PIN_NUMBER);//toggle the led
 
 }
 
