a64cbc6ff070cb58fdf75c2a0ab4b7f90edf6ec5
N1r2v3 <100696606+N1r2v3@users.noreply.github.com>
2022-05-18 19:45:59 +0200

    Update counterMain.c

--- counterMain.c
+++ counterMain.c
@@ -1,31 +1,85 @@
-#include "counterMain.h" 
+#include "LCDdriver.h"
+#include "timerA.h" 
+#include "GPIO.h"
+#include "additionalFunctions.h"
+#include "led.h"
+#include "CountDown.h"
 // input is array of char arranged where minutes tenth is the first value then munites unitth then seconds tenth then seconds units 
 // example 0130 is 1 minute and 30 seconds
-
-
-
-void Count(){
-
+unsigned char countArray[4];
+void displaycount(){
+	  LCD_ClearScreen();
+	  LCD_Character(countArray[0]);
+		LCD_Character(countArray[1]);
+		LCD_Character(':');
+		LCD_Character(countArray[2]);
+		LCD_Character(countArray[3]);
+	
+}
+void Count(void){
+int displayone,switches ,i;
 LCD_Init();
-LCD_String(countArray);
 Timer_Init();
+switchesInit();	
+start_func();
 
+displayone=0;	// initialize all functions
+  	
 while (1)
-	{
+	{  while (READ_PIN_VALUE( 'E',  4)==1){ // check door
+	
+		if (displayone==0){
+			displayone=1;
+	displaycount();}
+		
+	} // while door open toggle
+	displayone=0;
+  while ( READ_PIN_VALUE( 'E',  5)==0){ 
+		// CHECK PAUSE
+	pause:	pause_func();
+				LCD_ClearScreen();
+		pause_func();
+		timer_wait();
+    	if (displayone==0){
+			displayone=1;
+	displaycount();}
+		while(1){
+			for(i=0;i<5;i++){
+			pause_func();}
+    if (  READ_PIN_VALUE( 'E',  5)==0 ){ // CHECK PAUSE FOR SECOND TIME TO STOP
+			stop_func(2);
+			break;
+    }
+		if (  READ_PIN_VALUE( 'F',  0)==0 ){ // CHECK PAUSE FOR SECOND TIME TO STOP
+			start_func();
+			break;
+    }
+		pause_func();
+	}
+  }
+  // read pause
+     
+		
+			
+
+		
+	displaycount();
+	for (i=0;i<10;i++){
+ if(READ_PIN_VALUE( 'E',  5)==0) goto pause;
+		timer_wait();
+	}
+
+		
+	if(READ_PIN_VALUE( 'E',  5)==0) goto pause;
 		if ( countArray[3] != '0') { countArray[3]--;}  // if seconds not equal zero minus seconds
 		else if (countArray[3]=='0' & countArray[2] !='0')   {countArray[2]--; countArray[3]='9';} // minus 10th of seconds
 		else if (countArray[3]=='0' & countArray[2] =='0' & countArray[1]!='0')   { countArray [3]='9' ; countArray[2]='5'; countArray[1]--;} // minus minutes
 		else if (countArray[3]=='0' & countArray[2] =='0' & countArray[1]=='0' & countArray[0] != '0')   { countArray [3]='9' ; countArray[2]='5'; countArray[1]='9' ; countArray[0]--;}
 		else if (countArray[3]=='0' & countArray[2] =='0' & countArray[1]=='0' & countArray[0] == '0' ) { break;}
-		LCD_Character(countArray[0]);
-		LCD_Character(countArray[1]);
-		LCD_Character(':');
-		LCD_Character(countArray[2]);
-		LCD_Character(countArray[3]);
-		timer_wait_oneSec();
-		LCD_ClearScreen();
-	}
+	
+	
 }
+	}
 
 void Countdown_doubletochar(double Time)		//Convert double value to an array of characters
 {
