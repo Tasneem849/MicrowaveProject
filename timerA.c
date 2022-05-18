a2a52a3b3b367f765a9ad0cbd77ec9e86c41a5dc
N1r2v3 <100696606+N1r2v3@users.noreply.github.com>
2022-05-18 17:29:44 +0200

    Update timerA.c

--- timerA.c
+++ timerA.c
@@ -4,14 +4,14 @@ void Timer_Init(void)
     SYSCTL_RCGCTIMER_R |= 0x01 ;// TIMERn =0//
      // enable 
 }
-void timer_wait_oneSec(void)
+void timer_wait(void)
 {
     // na2es lw hashaghal interrupt
     TIMER0_CTL_R &= ~(1<<0);  // disable timer count
     TIMER0_CFG_R = 0x0000000000; /// set conf=0
     TIMER0_TAMR_R  |= 0x02; // enabling periodic mode
     TIMER0_TAMR_R  &= ~(1<<4); // enabling count down 
-    TIMER0_TAILR_R = 0x00F42400; // timer is set to one second
+    TIMER0_TAILR_R = 0x186A00; // timer is set to one second
     // interrupt step here//
     TIMER0_CTL_R |= (1<<0); // enabling timer count
     while (1)
@@ -27,8 +27,8 @@ void timer_wait_oneSec(void)
 void timer_wait_inSec(int number)
 {
 	int i;
-    for(i=0 ;i<number;i++)
+    for(i=0 ;i<10*number;i++)
     {
-        timer_wait_oneSec();
+        timer_wait();
     }
 }
