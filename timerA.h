7c075702b45075fb6f0a0031c897525f4b3a4a27
N1r2v3 <100696606+N1r2v3@users.noreply.github.com>
2022-05-18 17:28:24 +0200

    Update timerA.h

--- timerA.h
+++ timerA.h
@@ -1,3 +1,3 @@
 void Timer_Init(void); // intialize timer A 
-void timer_wait_oneSec(void); // start timer for 1 second
+void timer_wait(void); // start timer for 1 second
 void timer_wait_inSec(int number); // timer in seconds
