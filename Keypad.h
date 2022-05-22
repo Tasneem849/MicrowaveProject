9e17ccb7a3d9a6b66fdf7b6e84711ae943d8be77
Radwa516 <104581260+Radwa516@users.noreply.github.com>
2022-05-22 15:08:39 +0200

    Update Keypad.h

--- Keypad.h
+++ Keypad.h
@@ -1,8 +1,8 @@
 #include "GPIO.h"
 #include "Button.h"
 
-#define KEYPAD_ROWS 'A'					//Rows are on pins D0, D1, D2, D3
-#define KEYPAD_COLUMNS 'C'      //Columns are on pins C4, C5, C6, C7
+#define KEYPAD_ROWS 'A'					//ROWS are on pins A4, A5, A6, A7
+#define KEYPAD_COLUMNS 'C'      //COLUMNS are on pins C4, C5, C6, C7
 
 
 void KEYPAD_INIT(void);
